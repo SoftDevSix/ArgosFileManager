d14741d17909ee6b1309e8951cfcc290371f5763
gabygabylagaby <gabrielagv7656@gmail.com>
2024-11-25 00:54:51 -0400

    feat: create base repository for file manager

---
+++ src/main/java/org/argos/argosfilemanager/ArgosFileManagerApplication.java
@@ -0,0 +1,13 @@
+package org.argos.argosfilemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgosFileManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArgosFileManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/argos/argosfilemanager/ArgosFileManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.argos.argosfilemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgosFileManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
